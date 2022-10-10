da0819b78e3be1247760dded78686ede20deb4bc
takashik726 <takashik@atmoph.com>
2022-10-10 19:31:14 +0900

    Change the method to show the splash screen from the activity to the SplashScreen-APIs

--- app/src/main/java/com/zakojifarm/farmapp/Constant.kt
+++ app/src/main/java/com/zakojifarm/farmapp/Constant.kt
@@ -1,15 +1,13 @@
 package com.zakojifarm.farmapp
 
-import com.zakojifarm.farmapp.utils.Utils
-
 class Constant {
     companion object {
-        const val SPLASH_SCREEN_DURATION = 1000L
+        const val SPLASH_SCREEN_DURATION = 3000L
 
-        val permissions = arrayOf(
-            android.Manifest.permission.INTERNET,
-            android.Manifest.permission.ACCESS_COARSE_LOCATION,
-            android.Manifest.permission.ACCESS_FINE_LOCATION
-        )
+//        val permissions = arrayOf(
+//            android.Manifest.permission.INTERNET,
+//            android.Manifest.permission.ACCESS_COARSE_LOCATION,
+//            android.Manifest.permission.ACCESS_FINE_LOCATION
+//        )
     }
 }
