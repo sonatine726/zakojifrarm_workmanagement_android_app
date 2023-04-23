ff9f42a1dbfa2960caa3b2893350500c3cb58510
takashik726 <takashik@atmoph.com>
2023-04-23 21:25:59 +0900

    First success to upload files to GoogleDrive

--- app/src/main/java/com/zakojifarm/farmapp/utils/GoogleDriveUtils.kt
+++ app/src/main/java/com/zakojifarm/farmapp/utils/GoogleDriveUtils.kt
@@ -1,8 +1,19 @@
 package com.zakojifarm.farmapp.utils
 
 import android.util.Log
+import com.google.api.client.googleapis.json.GoogleJsonResponseException
+import com.google.api.client.http.FileContent
+import com.google.api.client.http.HttpRequestInitializer
+import com.google.api.client.http.javanet.NetHttpTransport
+import com.google.api.client.json.gson.GsonFactory
+import com.google.api.services.drive.Drive
+import com.google.auth.http.HttpCredentialsAdapter
 import com.google.auth.oauth2.GoogleCredentials
 import com.zakojifarm.farmapp.MainApplication
+import java.io.File
+import java.io.FileWriter
+import java.io.IOException
+import java.util.*
 
 
 class GoogleDriveUtils {
@@ -12,69 +23,45 @@ class GoogleDriveUtils {
         fun uploadBarcodeFileToDrive() {
             Log.v(TAG, "uploadBarcodeFileToDrive")
             val credentials =
-                GoogleCredentials.fromStream(MainApplication.instance.assets.open("zakojifarmapp-1b66497aedf8.json"))
+                GoogleCredentials.fromStream(MainApplication.instance.assets.open("zakojifarmapp-0d33671abfa7.json"))
+                    .createScoped("https://www.googleapis.com/auth/drive")
 
-//            credentials.refreshIfExpired()
-//            Log.v(TAG, "TesTes.$credentials\n${credentials.accessToken}\n")
+            val requestInitializer: HttpRequestInitializer = HttpCredentialsAdapter(
+                credentials
+            )
 
+            // Build a new authorized API client service.
+            val service = Drive.Builder(
+                NetHttpTransport(),
+                GsonFactory.getDefaultInstance(),
+                requestInitializer
+            )
+                .setApplicationName("ZakojiFarmApp")
+                .build()
 
-            //        credentials.refreshIfExpired()
-//        val token = credentials.accessToken
+            // Upload file photo.jpg on drive.
+            val fileMetadata = com.google.api.services.drive.model.File().apply {
+                name = "test.txt"
+                parents = Collections.singletonList("1K6FDlRYgsGVMJFF8uJoa_ErIckxko2CX")
+            }
 
-////        val resourceAsStream: InputStream = AuthTest::class.java.getClassLoader()
-//            .getResourceAsStream("Google-Play-Android-Developer.json")
-//
-////        val credential = GoogleCredential.fromStream(resourceAsStream)
-//        val credential = GoogleCredentials.fromStream(resourceAsStream)
-//
-//        // Get service account secret
-//        val inputStream = MainActivity::class.java.getResourceAsStream(CREDENTIALS_FILE_PATH)
-//            ?: throw FileNotFoundException("Resource not found: $CREDENTIALS_FILE_PATH")
-//
-//        // Convert inputStream to file
-//        val clientSecret: File = File(getFilesDir() + File.separator + "credentials.p12")
-//        val outputStream: OutputStream = FileOutputStream(clientSecret)
-//        IOUtils.copy(inputStream, outputStream)
-//        if (!clientSecret.exists()) throw FileNotFoundException("Credentials (credentials.p12) not created from: $CREDENTIALS_FILE_PATH")
-//
-//        // Http transport creation
-//        val httpTransport: HttpTransport = AndroidHttp.newCompatibleTransport()
-//        // Instance of the JSON factory
-//        val jsonFactory: JsonFactory = JacksonFactory.getDefaultInstance()
-//        // Instance of the scopes required
-//        val scopes: MutableList<String> = ArrayList()
-//        scopes.add(DriveScopes.DRIVE)
-//        // Build Google credential
-//        val credential = GoogleCredential.Builder()
-//            .setTransport(httpTransport)
-//            .setJsonFactory(jsonFactory)
-//            .setServiceAccountId(SERVICE_ACCOUNT_PROVIDER)
-//            .setServiceAccountScopes(scopes)
-//            .setServiceAccountPrivateKeyFromP12File(clientSecret)
-//            .setServiceAccountUser(SERVICE_ACCOUNT_USER)
-//            .build()
-//        // Build Drive service
-//        val service: Drive = Builder(httpTransport, jsonFactory, credential)
-//            .setApplicationName(APPLICATION_NAME)
-//            .build()
-//
-//        // Parents
-//        val parents: MutableList<String> = ArrayList()
-//        parents.add(TARGET_FOLDER_ID)
-//        // Setup file
-//        val timeStamp: String =
-//            SimpleDateFormat("yyyyMMddHHmmss", Locale.forLanguageTag("hu-HU")).format(
-//                Date()
-//            )
-//        val fileMetadata = File()
-//        fileMetadata.setName("scan_$timeStamp.txt")
-//        fileMetadata.setParents(parents)
-//        val filePath: File = File(getFilesDir() + File.separator + BARCODE_CONTAINER_TEMP_FILE)
-//        val mediaContent = FileContent("text/plain", filePath)
-//        // Upload file to google drive
-//        service.files().create(fileMetadata, mediaContent)
-//            .setFields("id")
-//            .execute()
+            val file = File(MainApplication.instance.cacheDir, "test.txt")
+            try {
+                FileWriter(file).use { writer -> writer.write("testes") }
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+
+            val mediaContent = FileContent("text/plain", file)
+
+            try {
+                val resultFile =
+                    service.files().create(fileMetadata, mediaContent).setFields("id").execute()
+                Log.v(TAG, "File ID: " + resultFile.id)
+            } catch (e: GoogleJsonResponseException) {
+                Log.v(TAG, "Unable to upload file: " + e.details)
+                throw e
+            }
         }
     }
 }
